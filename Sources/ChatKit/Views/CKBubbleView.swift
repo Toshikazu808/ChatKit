f49d80c3f327c87ad8b7101aaec6a941f59d8df9
Ryan Kanno <ryantkanno@gmail.com>
2025-11-10 14:31:02 -0800

    Add CKBubbleView to package.

---
+++ Sources/ChatKit/Views/CKBubbleView.swift
@@ -0,0 +1,29 @@
+//
+//  SwiftUIView.swift
+//  ChatKit
+//
+//  Created by Ryan Kanno on 11/10/25.
+//
+
+import SwiftUI
+
+struct CKBubbleView: View {
+    let message: CKMessage
+    let fromUser: Bool
+    
+    var body: some View {
+        let msg = message.message
+        Text(msg)
+            .font(.title3)
+            .padding(8)
+            .background {
+                // TODO: - Update colors from view model
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(fromUser ? Color.blue : Color.gray)
+            }
+    }
+}
+
+#Preview {
+    CKBubbleView(message: CKMessage(id: "abc123", chatGroupId: "0yvg4g52h8", date: .now, senderId: "98nrvtgvt", senderName: "Ryan Kanno", message: "Hi, are you available to take a job?  I wanted to get some help fixing my sink.", jwt: "", tpc: ""), fromUser: true)
+}
