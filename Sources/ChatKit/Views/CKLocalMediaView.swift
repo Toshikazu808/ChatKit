b7e979bc833ae7cfab788c5bc5634fa8fea005f1
Ryan Kanno <ryantkanno@gmail.com>
2025-11-10 14:23:20 -0800

    Add CKLocalMediaView to package.

---
+++ Sources/ChatKit/Views/CKLocalMediaView.swift
@@ -0,0 +1,58 @@
+//
+//  SwiftUIView.swift
+//  ChatKit
+//
+//  Created by Ryan Kanno on 11/10/25.
+//
+
+import SwiftUI
+import AVKit
+
+struct CKLocalMediaView: View {
+    let media: [CKAVSendable]
+    let selectedMedia: CKAVSendable
+    private let players: [AVPlayer?]
+    
+    init(media: [CKAVSendable], selectedMedia: CKAVSendable) {
+        self.media = media
+        self.selectedMedia = selectedMedia
+        self.players = media.map({ av in
+            guard let movie = av.movie else { return nil }
+            return AVPlayer(url: movie.localUrl)
+        })
+    }
+    
+    var body: some View {
+        ZStack {
+            Color.white
+                .ignoresSafeArea()
+            
+            TabView {
+                ForEach(0..<media.count, id: \.self) { i in
+                    if media[i].movie != nil {
+                        if let player = players[i] {
+                            VideoPlayer(player: player)
+                                .clipped()
+                                .ignoresSafeArea()
+                        } else {
+                            Image(systemName: "photo.badge.arrow.down")
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                        }
+                    } else {
+                        Image(uiImage: media[i].image)
+                            .resizable()
+                            .ignoresSafeArea()
+                            .scaledToFit()
+                    }
+                }
+            }
+            .tabViewStyle(.page)
+        }
+    }
+}
+
+#Preview {
+    CKLocalMediaView(media: [], selectedMedia: CKAVSendable.empty())
+}
