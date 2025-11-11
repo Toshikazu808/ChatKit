911bd81fe74fca0f817b392139a1cff348ec8437
Ryan Kanno <ryantkanno@gmail.com>
2025-11-11 13:02:58 -0800

    Add CKMessageScrollView to package.

---
+++ Sources/ChatKit/Views/CKMessageScrollView.swift
@@ -0,0 +1,48 @@
+//
+//  SwiftUIView.swift
+//  ChatKit
+//
+//  Created by Ryan Kanno on 11/11/25.
+//
+
+import SwiftUI
+
+struct CKMessageScrollView: View {
+    @Binding var messages: [CKMessage]
+    let userId: String
+    let onTapMessage: (CKMessage) -> Void
+    let onTapMedia: (CKMessage, CKMediaUrl) -> Void
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let w = geometry.size.width
+            ScrollViewReader { proxy in
+                ScrollView {
+                    LazyVStack(spacing: 10) {
+                        Rectangle()
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 100)
+                            .foregroundStyle(.clear)
+                        
+                        ForEach(Array(messages.enumerated()), id: \.offset) { i, message in
+                            CKMessageView(message: message, userId: userId, screenWidth: w, onTapMessage: { message in
+                                onTapMessage(message)
+                            }, onTapMedia: { message, selectedMedia in
+                                onTapMedia(message, selectedMedia)
+                            })
+                            .id(i)
+                        }
+                    }
+                }
+                .scrollContentBackground(.hidden)
+                .onChange(of: messages.count) { _, _ in
+                    proxy.scrollTo(messages.count - 1)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CKMessageScrollView(onTapMessage: { _ in }, onTapMedia: { _, _ in })
+}
