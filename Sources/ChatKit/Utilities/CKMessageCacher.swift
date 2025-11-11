174a98c965e06279d8fc440c323469447737167b
Ryan Kanno <ryantkanno@gmail.com>
2025-11-10 21:21:00 -0800

    Add CKMessageCacher to package.

---
+++ Sources/ChatKit/Utilities/CKMessageCacher.swift
@@ -0,0 +1,55 @@
+//
+//  File.swift
+//  ChatKit
+//
+//  Created by Ryan Kanno on 11/10/25.
+//
+
+import Foundation
+import SwiftData
+
+protocol CKMessageCacherProtocol {
+    func fetchCachedMessage(for chatGroupId: String) -> [CKMessage]
+    func cacheMessages(_ messages: [CKMessage]) throws
+}
+
+struct CKMessageCacher: CKMessageCacherProtocol {
+    let modelContext: ModelContext
+    private let cacheLimit = 100
+    
+    func fetchCachedMessage(for chatGroupId: String) -> [CKMessage] {
+        do {
+            let descriptor = FetchDescriptor<CKCachedMessage>(
+                predicate: #Predicate { $0.chatGroupId == chatGroupId },
+                sortBy: [SortDescriptor(\.date)])
+            let messages = try modelContext.fetch(descriptor)
+            return messages.map({ CKMessage(cachedMessage: $0) })
+        } catch {
+            return []
+        }
+    }
+    
+    func cacheMessages(_ messages: [CKMessage]) throws {
+        guard !messages.isEmpty else { return }
+        let messages = messages.map({ CKCachedMessage(message: $0) })
+        try removeOld(messages)
+        try modelContext.transaction {
+            messages.forEach {
+                modelContext.insert($0)
+            }
+            try modelContext.save()
+        }
+    }
+    
+    private func removeOld(_ messages: [CKCachedMessage]) throws {
+        guard messages.count > cacheLimit else { return }
+        let prefix = messages.count - cacheLimit
+        let toDelete = Array(messages.prefix(prefix))
+        try modelContext.transaction {
+            toDelete.forEach {
+                modelContext.delete($0)
+            }
+            try modelContext.save()
+        }
+    }
+}
