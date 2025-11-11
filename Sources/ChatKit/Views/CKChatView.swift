37d7e3eaedb7c245a0b11b5419de0de628847be3
Ryan Kanno <ryantkanno@gmail.com>
2025-11-11 15:07:42 -0800

    Add CKChatView to package.

---
+++ Sources/ChatKit/Views/CKChatView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  ChatKit
+//
+//  Created by Ryan Kanno on 11/11/25.
+//
+
+import SwiftUI
+
+struct CKChatView: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CKChatView()
+}
