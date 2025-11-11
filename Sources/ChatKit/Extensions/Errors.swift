05c94f6fbdfa8dae567e57dc1933f2d4e16e430b
Ryan Kanno <ryantkanno@gmail.com>
2025-11-10 21:43:03 -0800

    Add custom Errors.

---
+++ Sources/ChatKit/Extensions/Errors.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  ChatKit
+//
+//  Created by Ryan Kanno on 11/10/25.
+//
+
+import Foundation
+
+public enum Errors: LocalizedError {
+    case noSelf
+    case keyNotFound(String, [String])
+    
+    public var errorDescription: String? {
+        switch self {
+        case .noSelf:
+            "Reference to self has been deinitialized."
+        case .keyNotFound(let object, let keys):
+            "Dictionary keys for \(object) not found: \(keys.joined(separator: ", "))"
+        }
+    }
+}
