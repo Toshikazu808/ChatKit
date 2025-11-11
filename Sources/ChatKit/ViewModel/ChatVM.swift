b7833061e5f22b597b42e9f23cfecd79dae7dd19
Ryan Kanno <ryantkanno@gmail.com>
2025-11-10 21:16:08 -0800

    Add ChatVM to package.

---
+++ Sources/ChatKit/ViewModel/ChatVM.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  ChatKit
+//
+//  Created by Ryan Kanno on 11/10/25.
+//
+
+import SwiftUI
+import Observation
+import PhotosUI
+
+protocol ChatVMDelegate: AnyObject {
+    
+}
+
+@Observable @MainActor final class ChatVM {
+    
+}
