baa43be6a422a11edf953e099946fcc4373e74c1
Ryan Kanno <ryantkanno@gmail.com>
2025-11-10 21:55:21 -0800

    Add properties to view model.

--- Sources/ChatKit/ViewModel/ChatVM.swift
+++ Sources/ChatKit/ViewModel/ChatVM.swift
@@ -14,5 +14,46 @@ protocol ChatVMDelegate: AnyObject {
 }
 
 @Observable @MainActor final class ChatVM {
+    let db: any CKMessageCacherProtocol
     
+    private let avp = CKAVProcessor.shared
+    private let cacheLimit = 100
+    
+    var messages: [CKMessage] = []
+    var tempMessagesCache: [CKMessage] = []
+    
+    var text = ""
+    var selectedMedia: [CKAVSendable] = []
+    var photoPickerItems: [PhotosPickerItem] = []
+    
+    var showCamera = false
+    var showCameraError = false
+    
+    var showPhotosPicker = false
+    
+    var showAuthorizationError = false
+    var authorizationError = ""
+    
+//    private(set) var speechManager: any SpeechManageable
+    var isRecording = false
+    
+    var displayError = false
+    var error = ""
+    
+    init(db: any CKMessageCacherProtocol) {
+        self.db = db
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(cacheMessages),
+            name: UIApplication.willTerminateNotification,
+            object: nil)
+    }
+    
+    @objc private func cacheMessages() {
+        do {
+            try db.cacheMessages(messages)
+        } catch {
+            return
+        }
+    }
 }
