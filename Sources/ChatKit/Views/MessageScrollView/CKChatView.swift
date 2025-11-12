4db2360ab3d80d0bb7f664898febc34a130c436f
Ryan Kanno <ryantkanno@gmail.com>
2025-11-12 10:14:22 -0800

    Add initializers to CKChatView.

--- Sources/ChatKit/Views/MessageScrollView/CKChatView.swift
+++ Sources/ChatKit/Views/MessageScrollView/CKChatView.swift
@@ -6,9 +6,38 @@
 //
 
 import SwiftUI
+import PhotosUI
+import SwiftData
 
 public struct CKChatView: View {
+    public let userId: String
+    public let userName: String
+//    @Environment(ChatGroupsVM.self) private var chatGroupsVM
+    public let chatGroup: CKChatGroup
+    @State public var vm: CKChatVM
+    private let bottomButtonPadding: CGFloat = 3
+    private let buttonSize: CGFloat = 25
     
+    enum Field: Hashable {
+        case chat
+    }
+    @FocusState var isKeyboardFocused: Field?
+    
+    init(userId: String, userName: String, chatGroup: CKChatGroup, modelContext: ModelContext) {
+        self.userId = userId
+        self.userName = userName
+        self.chatGroup = chatGroup
+        let db = CKMessageCacher(modelContext: modelContext)
+        let vm = CKChatVM(userId: userId, db: db)
+        self._vm = State(wrappedValue: vm)
+    }
+    
+    init(userId: String, userName: String, chatGroup: CKChatGroup, vm: CKChatVM) {
+        self.userId = userId
+        self.userName = userName
+        self.chatGroup = chatGroup
+        self._vm = State(wrappedValue: vm)
+    }
     
     public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
