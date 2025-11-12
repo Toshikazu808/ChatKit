637825e008d10c29ff3ac5958e31a89a5e266b9a
Ryan Kanno <ryantkanno@gmail.com>
2025-11-12 11:15:19 -0800

    Add CKChatButton to package.

---
+++ Sources/ChatKit/Views/MessageScrollView/CKChatButton.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUIView.swift
+//  ChatKit
+//
+//  Created by Ryan Kanno on 11/12/25.
+//
+
+import SwiftUI
+
+public struct CKChatButton: View {
+    public let type: `Type`
+    public let size: CGFloat
+    public let action: () -> Void
+    
+    public enum `Type`: String {
+        case stop = "stop.circle"
+        case send = "paperplane"
+        case mic, photo, camera
+    }
+    
+    public var body: some View {
+        Button(action: action) {
+            Image(systemName: type.rawValue)
+                .resizable()
+                .scaledToFit()
+                .frame(width: size, height: size)
+        }
+    }
+}
+
+#Preview {
+    CKChatButton(type: .photo, size: 25, action: {})
+}
