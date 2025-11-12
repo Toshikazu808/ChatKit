43bb5e61c011b326a396cbc2813fbde2bc5228fe
Ryan <ryantkanno@gmail.com>
2025-11-12 00:03:20 -0800

    Add properties to view model.

--- Sources/ChatKit/ViewModels/CKChatGroupsVM.swift
+++ Sources/ChatKit/ViewModels/CKChatGroupsVM.swift
@@ -8,6 +8,15 @@
 import Foundation
 import Observation
 
-@Observable @MainActor final class CKChatGroupsVM {
+@Observable @MainActor public final class CKChatGroupsVM {
+    public var viewDidLoad = false
+    public var navPath: [CKChatsNavPath] = []
+    public var isLoading = false
+    public var chatGroups: [CKChatGroup] = []
+    public var archivedChats: [CKChatGroup] = []
+    public private(set) var didFetchBatch = false
+    public private(set) var didFetchArchivedBatch = false
+    public var showAlert = false
+    
     
 }
