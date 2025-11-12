b89c9c7205b964a5d1e44caba994627211ce15aa
Ryan Kanno <ryantkanno@gmail.com>
2025-11-12 08:38:35 -0800

    Add CKChatsRootView to package.

---
+++ Sources/ChatKit/Views/ChatGroupsListView/CKChatsRootView.swift
@@ -0,0 +1,20 @@
+//
+//  CKChatsRootView.swift
+//  ChatKit
+//
+//  Created by Ryan Kanno on 11/12/25.
+//
+
+import SwiftUI
+
+struct CKChatsRootView: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CKChatsRootView()
+}
