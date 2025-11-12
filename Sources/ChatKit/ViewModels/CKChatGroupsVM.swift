d991f2be1dfe71a6418db50a34fcb486e5ac1865
Ryan <ryantkanno@gmail.com>
2025-11-11 23:59:34 -0800

    Add CKChatGroupsVM to package.

---
+++ Sources/ChatKit/ViewModels/CKChatGroupsVM.swift
@@ -0,0 +1,13 @@
+//
+//  CKChatGroupsVM.swift
+//  ChatKit
+//
+//  Created by Ryan Kanno on 11/11/25.
+//
+
+import Foundation
+import Observation
+
+@Observable @MainActor final class CKChatGroupsVM {
+    
+}
