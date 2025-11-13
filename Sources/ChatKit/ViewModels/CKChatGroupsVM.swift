be7ee709d0b0b322a65d65c2523f39f39bb95d5d
Ryan Kanno <ryantkanno@gmail.com>
2025-11-13 15:26:25 -0800

    Add navigation functions.

--- Sources/ChatKit/ViewModels/CKChatGroupsVM.swift
+++ Sources/ChatKit/ViewModels/CKChatGroupsVM.swift
@@ -18,5 +18,28 @@ import Observation
     public private(set) var didFetchArchivedBatch = false
     public var showAlert = false
     
+    public var chatGroupComparable: (any CKChatGroupComparable)?
+    public internal(set) var shouldOpenChat = false
     
+    
+    
+    public func openChatGroup(for chatGroupComparable: any CKChatGroupComparable) {
+        if let chat = chatGroups.first(where: { $0.id == chatGroupComparable.id }) {
+            self.chatGroupComparable = chatGroupComparable
+            navPath.append(.messages(chat))
+        }
+    }
+    
+    public func openChatIfNeeded() {
+        guard let chatGroupComparable, shouldOpenChat else { return }
+        navPath.removeAll()
+        openChatGroup(for: chatGroupComparable)
+        shouldOpenChat = false
+    }
+    
+    public func resetArchivedChats() {
+        if !navPath.contains(.archived) {
+            archivedChats.removeAll()
+        }
+    }
 }
