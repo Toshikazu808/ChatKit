71bb676db296d15b267a304a6d95dabca4971995
Ryan Kanno <ryantkanno@gmail.com>
2025-11-14 13:41:01 -0800

    Add navigation logic.

--- Sources/ChatKit/Views/ChatGroupsListView/CKChatsRootView.swift
+++ Sources/ChatKit/Views/ChatGroupsListView/CKChatsRootView.swift
@@ -6,15 +6,50 @@
 //
 
 import SwiftUI
+import SwiftData
 
-struct CKChatsRootView: View {
+/// > Important: Remember to inject a `CKChatGroupsVM` into the environment using `.environment(vm)`.
+public struct CKChatsRootView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Environment(CKChatGroupsVM.self) private var vm
+    public let userId: String
+    public let userName: String
     
-    
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    public var body: some View {
+        @Bindable var vm = vm
+        NavigationStack(path: $vm.navPath) {
+            CKChatGroupsListView()
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationTitle("Messages")
+                .toolbar { CKArchivedButton() }
+                .navigationDestination(for: CKChatsNavPath.self) { path in
+                    switch path {
+                    case .messages(let chatGroup):
+                        CKChatView(userId: userId, userName: userName, chatGroup: chatGroup, modelContext: modelContext, viewDidAppear: { chatGroup in
+                            if let id = vm.chatGroupComparable?.id, id.isEmpty {
+                                Task {
+                                    try await vm.fetchChatGroupComparable(for: id)
+                                }
+                            }
+                        }, viewDidDisappear: { _ in
+                            vm.chatGroupComparable = nil
+                        }, navPath: $vm.navPath)
+                        .navigationBarTitleDisplayMode(.inline)
+                    case .mediaView(let media):
+                        CKLocalMediaCarouselView(media: media)
+                            .navigationBarTitleDisplayMode(.inline)
+                    case .remoteMediaView(let msg):
+                        CKRemoteMediaCarouselView(message: msg)
+                            .navigationBarTitleDisplayMode(.inline)
+                    case .archived:
+                        CKArchivedChatGroupsListView(userId)
+                            .navigationBarTitleDisplayMode(.inline)
+                    }
+                }
+        }
     }
 }
 
 #Preview {
-    CKChatsRootView()
+    CKChatsRootView(userId: "abc123", userName: "Joe Schmoe")
 }
