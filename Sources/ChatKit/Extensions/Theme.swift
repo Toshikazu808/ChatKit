9b68da752ace6e05da4dee31d86bd2f0ffed6d3e
Ryan Kanno <ryantkanno@gmail.com>
2025-11-15 20:55:50 -0800

    Add CKColorThemeConfig to package.

--- Sources/ChatKit/Extensions/Theme.swift
+++ Sources/ChatKit/Extensions/Theme.swift
@@ -13,3 +13,25 @@ import SwiftUI
     public static var silver = Color(red: 0.043, green: 0.298, blue: 0.376)
     public static var lightNavy = Color(red: 0.043, green: 0.298, blue: 0.376)
 }
+
+@MainActor public struct CKColorThemeConfig {
+    public private(set) var babyBlue: Color?
+    public private(set) var lightGray: Color?
+    public private(set) var silver: Color?
+    public private(set) var lightNavy: Color?
+    
+    public func setColorTheme() {
+        if let babyBlue {
+            Theme.babyBlue = babyBlue
+        }
+        if let lightGray {
+            Theme.lightGray = lightGray
+        }
+        if let silver {
+            Theme.silver = silver
+        }
+        if let lightNavy {
+            Theme.lightNavy = lightNavy
+        }
+    }
+}
