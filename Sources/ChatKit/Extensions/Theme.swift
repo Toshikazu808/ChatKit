5ee7b6ae3b0250d34f39d58e43b0f80a8d9758c6
Ryan Kanno <ryantkanno@gmail.com>
2025-11-15 21:02:36 -0800

    Adjust setter scope.

--- Sources/ChatKit/Extensions/Theme.swift
+++ Sources/ChatKit/Extensions/Theme.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @MainActor public enum Theme {
-    public static var babyBlue = Color(red: 0.941, green: 0.98, blue: 0988)
-    public static var lightGray = Color(red: 0.839, green: 0.839, blue: 0.839)
-    public static var silver = Color(red: 0.043, green: 0.298, blue: 0.376)
-    public static var lightNavy = Color(red: 0.043, green: 0.298, blue: 0.376)
+    public internal(set) static var babyBlue = Color(red: 0.941, green: 0.98, blue: 0988)
+    public internal(set) static var lightGray = Color(red: 0.839, green: 0.839, blue: 0.839)
+    public internal(set) static var silver = Color(red: 0.043, green: 0.298, blue: 0.376)
+    public internal(set) static var lightNavy = Color(red: 0.043, green: 0.298, blue: 0.376)
 }
 
 @MainActor public struct CKColorThemeConfig {
