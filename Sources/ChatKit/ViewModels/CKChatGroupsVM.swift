182bd7f544d9c1f4c90a31800be138140bad3953
Ryan Kanno <ryantkanno@gmail.com>
2025-11-15 16:17:06 -0800

    Set on MainActor and conform to Sendable.

--- Sources/ChatKit/ViewModels/CKChatGroupsVM.swift
+++ Sources/ChatKit/ViewModels/CKChatGroupsVM.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Observation
 
-public protocol CKChatGroupsVMApiDelegate: AnyObject, Sendable {
+@MainActor public protocol CKChatGroupsVMApiDelegate: AnyObject, Sendable {
     /// > Important: Remember to set this variable to `nil` in the `deinit` of the class conforming to `CKChatGroupsVMApiDelegate`.  `weak` variables can't be declared in protocols so we need to manage this memory manually to prevent retain cycles.
     var chatGroupsApiSubscriber: (any CKChatGroupsApiSubscriber)? { get set }
     
@@ -18,7 +18,7 @@ public protocol CKChatGroupsVMApiDelegate: AnyObject, Sendable {
     func archive(_ chatGroupComparable: any CKChatGroupComparable) async throws
 }
 
-@MainActor public protocol CKChatGroupsApiSubscriber: AnyObject {
+@MainActor public protocol CKChatGroupsApiSubscriber: AnyObject, Sendable {
     func didFetch(_ chatGroup: CKChatGroup)
 }
 
