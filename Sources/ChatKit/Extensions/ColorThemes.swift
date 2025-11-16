88dad980b9e48a19ca5ce951673df6c44f8f7314
Ryan Kanno <ryantkanno@gmail.com>
2025-11-15 19:45:13 -0800

    Add ColorThemes to package.

---
+++ Sources/ChatKit/Extensions/ColorThemes.swift
@@ -0,0 +1,13 @@
+//
+//  ColorThemes.swift
+//  ChatKit
+//
+//  Created by Ryan Kanno on 11/15/25.
+//
+
+import SwiftUI
+
+@MainActor internal enum ColorThemes {
+    static var userMessageBubbleViewColor: Color = .blue
+    static var otherUserMessageBubbleViewColor: Color = .green
+}
